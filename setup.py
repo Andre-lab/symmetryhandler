fd6eab9e717c484a0e4ca5d7ff91181a44411901
Mads Jeppesen <mads.jeppesen@biochemistry.lu.se>
2021-02-03 12:07:18 +0100

    Added a setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='symmetryhandler',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'symmetryhandler'},
+    url='https://github.com/Andre-lab/symmetryhandler',
+    license='MIT',
+    author='mads',
+    author_email='mads.jeppesen@biochemistry.lu.se',
+    description='Simple utility library to handcraft symmetry files in Rosetta and visualizing them in PyMOL '
+)
